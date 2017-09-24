05c8927139a7fe77623d8d6a3b26fdb9d997515b
baisheng <baisheng@outlook.com>
2017-09-24 23:07:39 +0800

    Add wechat route

--- server/api/routes-wechat.js
+++ server/api/routes-wechat.js
@@ -4,13 +4,16 @@
 import koaRouter from 'koa-router'
 import consts from '../utils/consts'
 import config from '../../api.config'
+// import querystring from 'querystring'
 import axios from 'axios'
-import OAuth from 'co-wechat-oauth'
+// import OAuth from 'co-wechat-oauth'
 // var OAuth = require('co-wechat-oauth');
 // var client = new OAuth('your appid', 'your secret');
 const fs = require('fs');
 // const readFile = util.promisify(fs.readFile);
 // const writeFile = util.promisify(fs.writeFile);
+// import WXBizDataCrypt from '../utils/WXBizDataCrypt'
+import Verification from '../lib/verification'
 
 const router = koaRouter({
   prefix: '/api'
@@ -19,11 +22,14 @@ const router = koaRouter({
 const request = axios.create({
   baseURL: config.baseURL
 })
-// router.get('/wechat', async (ctx) => {
-// })
-router.post('/app/:appid/login', async(ctx) => {
+const getWxAppUserInfo = (encrypted_data, iv, code) => {
+
+}
+
+router.post('/app/:appid/login', async (ctx) => {
+  // console.log(count++)
   const data = ctx.request.body
-  console.log(data)
+  // console.log(data)
   let option = await ctx.redis.get(`picker_${ctx.params.appid}_options`)
   if (option === null) {
     const response = await request.get(`/app/${ctx.params.appid}/options`)
@@ -31,24 +37,17 @@ router.post('/app/:appid/login', async(ctx) => {
   }
   if (option !== null) {
     option = JSON.parse(option)
-    const wechat = option.wechat
-    // console.log(JSON.stringify(wechat))
-    // 69ef69490d90f8c51c20039f6e29fb4e
-    let client = new OAuth('wxca1f2b8b273d909e', '69ef69490d90f8c51c20039f6e29fb4e')
-    // await ctx.session('code', data.code)
-    // await client.getAuthorizeURL(`${config.baseURL}/api/app/${ctx.params.appid}/openid`, 'state', 'scope');
-    console.log(data.code)
-    const token = await client.getAccessToken(data.code);
-    const accessToken = token.data.access_token;
-    const openid = token.data.openid;
-
-    console.log(token + ':' + accessToken + ':' + openid)
-    return token
-    // await client.getAuthorizeURL(`${config.baseURL}/api/app/${ctx.params.appid}/openid`, 'state', 'scope');
-    // console.log(client)
+    const wx = option.wechat
+    const verify = new Verification(wx.appid, wx.appsecret)
+    let userInfo = await verify.getUserInfo(data.username, data.password, data.code)
+    userInfo.approach = 'wxapp'
+    const regUser = await request.post(`/app/${ctx.params.appid}/users`, userInfo)
+    console.log(regUser)
   }
+  ctx.status = 200
   // console.log(data)
 })
+// router.post('/app/:appid/autho/token?code')
 router.get('/app/:appid/wechat', async (ctx) => {
   let option = await ctx.redis.get(`picker_${ctx.params.appid}_options`)
   if (option === null) {
@@ -57,11 +56,13 @@ router.get('/app/:appid/wechat', async (ctx) => {
   }
   if (option !== null) {
     option = JSON.parse(option)
-    const wechat = option.wechat
+    const wx = option.wechat
+    const verify = new Verification(wx.appid, wx.appsecret)
+    await verify.getSessionKey()
     // console.log(JSON.stringify(wechat))
-    let client = new OAuth(wechat.appid, wechat.appsecret)
-    await client.getAuthorizeURL(`${config.baseURL}/api/app/${ctx.params.appid}/openid`, 'state', 'scope');
-    console.log(client)
+    // let client = new OAuth(wechat.appid, wechat.appsecret)
+    // await client.getAuthorizeURL(`${config.baseURL}/api/app/${ctx.params.appid}/openid`, 'state', 'scope');
+    // console.log(client)
   }
 
   ctx.body = Object.assign({}, {'hi': 'hello'})
---
+++ server/api/verification.js
@@ -0,0 +1,7 @@
+import WXBizDataCrypt from '../utils/WXBizDataCrypt'
+
+const getWxAppUserInfo = (config, encrypted_data, iv, code) => {
+  // let appId = config.appid
+  // let secret = config.secret
+
+}
