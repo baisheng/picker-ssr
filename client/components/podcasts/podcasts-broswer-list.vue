5449551c0883f1be2d4b6b467545f6c87daa1520
baisheng <baisheng@outlook.com>
2017-10-10 15:29:00 +0800

    update

--- client/components/podcasts/podcasts-broswer-list.vue
+++ client/components/podcasts/podcasts-broswer-list.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="podcasts-borwser-list">
+  <div class="podcasts-browser-list">
     <div class="card section-header is-compact">
       <div class="section-header__label"><span
         class="section-header__label-text">{{term.name}}</span>
       </div>
       <div class="section-header__actions">
-        <nuxt-link class="button is-link podcasts-borwser-list__select-all" :to="'/podcasts/term/' + term.slug" v-if="expandedLink">
+        <nuxt-link class="button is-link podcasts-browser-list__select-all" :to="'/podcasts/term/' + term.slug"
+                   v-if="expandedLink">
           查看全部
           <svg class="gridicon gridicons-chevron-right" height="18" width="18"
                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -14,18 +15,28 @@
                 d="M10 20l8-8-8-8-1.414 1.414L15.172 12l-6.586 6.586"></path>
             </g>
           </svg>
-        </nuxt-link></div>
+        </nuxt-link>
+      </div>
     </div>
-    <div class="card podcasts-borwser-list__elements">
-      <li class="podcasts-borwser-item" v-for="item in list" :key="item.id">
-        <nuxt-link :to="'/podcast/' + item.id" class="podcasts-borwser-item__link">
-          <div class="podcasts-borwser-item__info">
+    <div class="card podcasts-browser-list__elements">
+      <li class="podcasts-browser-item" v-for="item in list" :key="item.id">
+        <nuxt-link :to="'/podcast/' + item.id" class="podcasts-browser-item__link">
+          <div class="podcasts-browser-item__info">
             <div class="plugin-icon">
               <img class="plugin-icon__img"
                    :src="item.featured_image">
             </div>
-            <div class="podcasts-borwser-item__title">{{item.title}}</div>
-            <div class="podcasts-borwser-item__author">{{ item.authorInfo.user_nicename }}</div>
+            <div class="podcasts-browser-item__title">{{item.title}}</div>
+            <div class="podcasts-browser-item__author">{{ item.authorInfo.user_nicename }}</div>
+            <div class="podcasts-browser-item__published" v-if="item.status === 'publish'">
+              <svg class="gridicon gridicons-checkmark" height="18" width="18" xmlns="http://www.w3.org/2000/svg"
+                   viewBox="0 0 24 24">
+                <g>
+                  <path d="M9 19.414l-6.707-6.707 1.414-1.414L9 16.586 20.293 5.293l1.414 1.414"></path>
+                </g>
+              </svg>
+              已发布
+            </div>
           </div>
         </nuxt-link>
       </li>
